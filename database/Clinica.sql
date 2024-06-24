14e0c49fa969a71714f0aecf5ce2a5b62f5fa352
Cookienator10 <santiagorojas772@gmail.com>
2024-06-24 08:16:04 -0500

    Corecciones

--- database/Clinica.sql
+++ database/Clinica.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema Clinica
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema Clinica
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `Clinica` DEFAULT CHARACTER SET utf8 ;
+USE `Clinica` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Paciente`
+-- Table `Clinica`.`Paciente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Paciente` (
+CREATE TABLE IF NOT EXISTS `Clinica`.`Paciente` (
   `ID_Paciente` INT NOT NULL,
   `Nombre` VARCHAR(45) NULL,
   `Direccion` VARCHAR(45) NULL,
@@ -28,9 +28,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Medico`
+-- Table `Clinica`.`Medico`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Medico` (
+CREATE TABLE IF NOT EXISTS `Clinica`.`Medico` (
   `ID_Medico` INT NOT NULL,
   `Nombre` VARCHAR(45) NULL,
   `Especialidad` VARCHAR(45) NULL,
@@ -41,9 +41,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Citas`
+-- Table `Clinica`.`Citas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Citas` (
+CREATE TABLE IF NOT EXISTS `Clinica`.`Citas` (
   `ID_Citas` INT NOT NULL,
   `MotivoCita` VARCHAR(88) NULL,
   `EstadoCita` ENUM("Confirmada", "Aplasada") NULL,
@@ -62,9 +62,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Citas` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Citas_Paciente_idx` ON `mydb`.`Citas` (`ID_Paciente` ASC) VISIBLE;
+CREATE INDEX `fk_Citas_Paciente_idx` ON `Clinica`.`Citas` (`ID_Paciente` ASC) VISIBLE;
 
-CREATE INDEX `fk_Citas_Medico1_idx` ON `mydb`.`Citas` (`ID_Medico` ASC) VISIBLE;
+CREATE INDEX `fk_Citas_Medico1_idx` ON `Clinica`.`Citas` (`ID_Medico` ASC) VISIBLE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
